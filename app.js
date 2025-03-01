db04483dd56af182c3455c349363ca374b6fbcff
Aratihiremath04 <hirematharati938@gmail.com>
2025-03-01 15:54:45 +0530

    product

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const morgan = require('morgan');
+const{Product}=require('./model/Product');
 
 
 //connecting to database
@@ -101,6 +102,46 @@ app.post('/login',async(req,res)=>{
     }
 })
 
+//task 3 create a route to see all product
+app.get('/products',async(req,res)=>{
+    try{
+        const products=await Product.find({});
+        return res.status(200).json({
+            message:"find all the products",
+            products:products
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:'Internal server error'})
+    }
+})
+
+//task 4 create a route to add a product
+app.post('/add-product',async(req,res)=>{
+    try{
+        const{name,price,image,brand,stock,description}=req.body;
+        const{token}=req.headers;
+
+        const decodedToken=jwt.verify(token,'supersecret');
+        const user=await User.findOne({email:decodedToken.email});
+
+        await Product.create({
+            name,
+            price,
+            description,
+            image,
+            stock,
+            brand,
+            user:user._id
+        });
+        return res.status(201).json({
+            message:"product added successfully"
+        })
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({message:'Internal server error'})
+    }
+})
 
 
 const PORT = 8080;

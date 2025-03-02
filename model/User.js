e3e23e7fa48cca7006647fe25827d3577e6058aa
Aratihiremath04 <hirematharati938@gmail.com>
2025-03-02 14:42:16 +0530

    task

--- model/User.js
+++ model/User.js
@@ -19,6 +19,10 @@ const userSchema=new mongoose.Schema({
     token:{
         type:String,
         required:true
+    },
+    cart:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Cart'
     }
 })  //to craete schema
 
